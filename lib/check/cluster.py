09748969b38b957adbe2b84cf06189c40b08d208
Jeroen van der Heijden <jeroen@cesbit.com>
2025-04-29 15:24:50 +0200

    Upd ver

--- lib/check/cluster.py
+++ lib/check/cluster.py
@@ -1,7 +1,5 @@
 import logging
-from libprobe import logger
 from libprobe.asset import Asset
-from libprobe.exceptions import CheckException
 from ..helpers import api_request
 
 
@@ -11,57 +9,55 @@ async def check_cluster(
         config: dict) -> dict:
     uri = '/api2/json/cluster/status'
     data = await api_request(asset, asset_config, config, uri)
-    try:
-        cluster = {}
-        nodes = []
-        backups = []
-        for item in data['data']:
-            if item['type'] == 'cluster':
-                cluster['name'] = item['name']  # str
-                cluster['nodes'] = item['nodes']  # int
-                cluster['version'] = item['version']  # int
-                cluster['quorate'] = item['quorate']  # int
-                cluster['id'] = item['id']  # str
-            elif item['type'] == 'node':
-                nodes.append({
-                    'name': item['name'],  # str
-                    'id': item['id'],  # str
-                    'ip': item['ip'],  # str
-                    'level': item['level'],  # str
-                })
 
-        uri = '/api2/json/cluster/ha/status/manager_status'
-        data = await api_request(asset, asset_config, config, uri)
+    cluster = {}
+    nodes = []
+    backups = []
+    for item in data['data']:
+        if item['type'] == 'cluster':
+            cluster['name'] = item['name']  # str
+            cluster['nodes'] = item['nodes']  # int
+            cluster['version'] = item['version']  # int
+            cluster['quorate'] = item['quorate']  # int
+            cluster['id'] = item['id']  # str
+        elif item['type'] == 'node':
+            nodes.append({
+                'name': item['name'],  # str
+                'id': item['id'],  # str
+                'ip': item['ip'],  # str
+                'level': item['leve'],  # str
+            })
 
-        lrm_status = data['data']['lrm_status']
-        node_status = data['data']['manager_status']['node_status']
-        for node in nodes:
-            item = lrm_status[node['name']]
-            node['mode'] = item['mode']  # str
-            node['state'] = item['state']  # str
-            node['status'] = node_status[node['name']]  # str
+    uri = '/api2/json/cluster/ha/status/manager_status'
+    data = await api_request(asset, asset_config, config, uri)
 
-        cluster['master_node'] = \
-            data['data']['manager_status']['master_node']  # str
+    lrm_status = data['data']['lrm_status']
+    node_status = data['data']['manager_status']['node_status']
+    for node in nodes:
+        item = lrm_status[node['name']]
+        node['mode'] = item['mode']  # str
+        node['state'] = item['state']  # str
+        node['status'] = node_status[node['name']]  # str
 
-        uri = '/api2/json/cluster/backup'
-        data = await api_request(asset, asset_config, config, uri)
-        for item in data['data']:
-            if item['type'] == 'vzdump':
-                backups.append({
-                    'name': item['id'],  # str
-                    'type': item['type'],  # str
-                    'schedule': item['schedule'],  # str
-                    'next_run': item['next-run'],  # int (unix timestamp)
-                    'mode': item['mode'],  # str
-                    'storage': item['storage'],  # str
-                    'enabled': bool(item['enabled']),  # int->bool
-                })
-            else:
-                logging.warning(f'unsupported backup type: {item["type"]}')
-    except Exception:
-        logger.exception()
-        raise
+    cluster['master_node'] = \
+        data['data']['manager_status']['master_node']  # str
+
+    uri = '/api2/json/cluster/backup'
+    data = await api_request(asset, asset_config, config, uri)
+    for item in data['data']:
+        if item['type'] == 'vzdump':
+            backups.append({
+                'name': item['id'],  # str
+                'type': item['type'],  # str
+                'schedule': item['schedule'],  # str
+                'next_run': item['next-run'],  # int (unix timestamp)
+                'mode': item['mode'],  # str
+                'storage': item['storage'],  # str
+                'enabled': bool(item['enabled']),  # int->bool
+            })
+        else:
+            logging.warning(f'unsupported backup type: {item["type"]}')
+
 
     return {
         'cluster': [cluster],
